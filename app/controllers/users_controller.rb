8addd22c55e114426f7f2b36bea4652aa7fac7a1
timkellogg <tim.kellogg@gmail.com>
2015-10-05 13:56:26 -0700

    update readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :is_admin?,      only: :destroy
+  
   def show
   	@user = User.find(params[:id])
   end
