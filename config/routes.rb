2e55be4b1d55926bbe858f554b6e03b0df159b26
timkellogg <tim.kellogg@gmail.com>
2015-10-05 20:41:07 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 	root :to      => 'static_pages#home'
 
-  get 'signup'  => 'users#new'
-  get  'login'  => 'sessions#new' 
-  post 'login'  => 'sessions#create'
-  get  'logout' => 'sessions#destroy'
+  get     'signup' => 'users#new'
+  get     'login'  => 'sessions#new' 
+  post    'login'  => 'sessions#create'
+  delete  'logout' => 'sessions#destroy'
 
   resources :users
 end
+
+
