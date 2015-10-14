6598b49a5ce4cdb0e4238b2c20dfe65e6f353afa
timkellogg <tim.kellogg@gmail.com>
2015-10-13 20:15:53 -0700

    refactor and improve styling of flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = "Welcome back, #{user.name}!"
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
