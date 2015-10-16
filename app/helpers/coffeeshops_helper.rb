67832a4632ef4a4d2c07487ed047bf02ba01a057
timkellogg <tim.kellogg@gmail.com>
2015-10-16 08:59:01 -0700

    refactor star rating presentation into helper

--- app/helpers/coffeeshops_helper.rb
+++ app/helpers/coffeeshops_helper.rb
@@ -1,16 +1,15 @@
 module CoffeeshopsHelper
-  # Eventually use this method in place of manually added in views
-  # to display fa_icons. 
+
   def calculate_stars(coffeeshop)
     output = ""
     total = coffeeshop.overall_average
 
     coffeeshop.overall_average.ceil.times do |n| 
       if total >= 1
-        output += content_tag(:i, "<%= fa_icon 'star' %>")
+        output += content_tag(:i, "", class: "fa fa-star")
         total -= 1
       else  
-        output += content_tag(:i, "<%= fa_icon 'star-half' %>")
+        output += content_tag(:i, "", class: "fa fa-star-half")
       end
     end
     return output
