7d37bd09f02d08b9bc686668c8e54e7ebfb628c0
timkellogg <tim.kellogg@gmail.com>
2015-10-23 11:33:27 -0700

    add integration testing of access to model resources

--- spec/features/authorization_spec.rb
+++ spec/features/authorization_spec.rb
@@ -1,49 +1,115 @@
 require 'rails_helper'
 require_relative '../support/utilities'
 
-describe "authorizations of the user", :type => :feature do 
+describe "authorizations of users", :type => :feature do 
 
   context "attempting to access coffeeshop resources" do 
+    
     describe "when not logged in" do 
-      it "does not allow the user to create/edit a coffeeshop" do  
+      it "does not allow the user to create a coffeeshop" do  
+        visit new_coffeeshop_path
+        expect(page).to have_content("ability")
+      end
+
+      it "does not allow the user to edit a coffeeshop" do  
         coffeeshop = FactoryGirl.create(:coffeeshop)
         visit coffeeshop_path(coffeeshop)
         expect(page).to_not have_content("Edit #{coffeeshop.name}")
         visit edit_coffeeshop_path(coffeeshop)
         expect(page).to have_content("ability")
       end
+
+      it "does not allow the user to destroy a coffeeshop" do 
+        coffeeshop = FactoryGirl.create(:coffeeshop)
+        visit coffeeshop_path(coffeeshop)
+        expect(page).to_not have_content("Remove?") 
+      end
     end
 
     describe "when logged in but not as an admin" do 
-      it "does not allow the user to create/edit a coffeeshop" do  
-        coffeeshop = FactoryGirl.create(:coffeeshop)
-        user = FactoryGirl.create(:user)
+      before do
+        @coffeeshop = FactoryGirl.create(:coffeeshop) 
+        user = FactoryGirl.create(:user) 
         log_in(user)
-        visit coffeeshop_path(coffeeshop)
-        expect(page).to_not have_content("Edit #{coffeeshop.name}")
+        visit new_coffeeshop_path(@coffeeshop)
+      end
+
+      it "does not allow the user to create a coffeeshop" do  
+        expect(page).to have_content("ability")
+      end
+
+      it "does not allow the user to edit a coffeeshop" do  
+        expect(page).to_not have_content("Edit #{@coffeeshop.name}")
+      end
+
+      it "does not allow the user to delete a coffeeshop" do  
+        expect(page).to_not have_content("Remove?")
       end
     end
 
     describe "when logged in as and admin" do 
-      it "does allow the user to create/edit a coffeeshop" do 
+      before do 
         user = FactoryGirl.create(:admin)
         log_in(user)
+      end
+
+      it "does allow the user to create a coffeeshop" do  
+        fill_in_coffeeshop_form
+        expect(page).to_not have_content("errors")
+        expect(Coffeeshop.count).to eq(1)
+      end
+
+      it "does allow the user to edit a coffeeshop" do 
         coffeeshop = FactoryGirl.create(:coffeeshop)
         visit coffeeshop_path(coffeeshop) 
         expect(page).to have_content("Edit #{coffeeshop.name}")
+        click_link "Edit #{coffeeshop.name}"
+        fill_in "Name", with: "New Name" 
+        click_button "List shop"
+        expect(page).to have_content("New Name")
+        expect(page).to_not have_content("#{coffeeshop.name}")
+        expect(Coffeeshop.count).to eq(1)
+      end
+
+      it "does allow the user to delete a coffeeshop" do  
+        coffeeshop = FactoryGirl.create(:coffeeshop)
+        visit coffeeshop_path(coffeeshop)
+        expect(page).to have_content("Remove?")
       end
     end
   end
 
-  context "attempting to access menu resources" do 
+  context "attempting to access menu and item resources" do 
     describe "when not logged in" do  
-      it "does not allow the user to create/delete a menu" do 
+      before do 
         coffeeshop = FactoryGirl.create(:coffeeshop)
         visit coffeeshop_path(coffeeshop)
+      end
+
+      it "does not allow the user to create a menu" do 
         expect(page).to_not have_content("Add a Menu")
         visit new_coffeeshop_menu_path(coffeeshop)
         expect(page).to_not have_content("Create")
         expect(page).to have_content("ability")
+      end
+    end
+
+    describe "when logged in as an admin" do  
+      before do 
+        coffeeshop = FactoryGirl.create(:coffeeshop)
+        visit coffeeshop_path(coffeeshop)
+      end
+
+      it "does allow the user to create a menu" do 
+        coffeeshop = FactoryGirl.create(:coffeeshop)
+        visit coffeeshop_path(coffeeshop)
+        click_button "Add a Menu"
+        click_button "Create?"
+        expect(page).to have_content("Menu was added")
+        click_button "Add Item"
+        fill_in_item_form 
+        expect(page).to_not have_content("errors")
+        expect(Item.count).to eq(1)
       end
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -9,3 +9,26 @@ def log_out
   visit root_path
   click_link 'Log out' 
 end
+
+def fill_in_coffeeshop_form
+  visit new_coffeeshop_path
+  fill_in "Name", with: "Example Coffeeshop"
+  fill_in "Address", with: "123 Main Street"
+  fill_in "City", with: "Faketown"
+  select "Alabama", from: "State"
+  fill_in "Website", with: "https://examplecoffeeshop.com/"
+  fill_in "Opens at", with: "8"
+  fill_in "Closes at", with: "8"
+  select "casual", from: "Style"
+  select "average", from: "How expensive?"
+  select "lots", from: "Parking"
+  click_button "List shop"
+end
+
+def fill_in_item_form 
+  fill_in "Name", with: "Coffee"
+  select "brunch", from: "Dish type?"
+  fill_in "Price", with "9"
+  fill_in "Description", with: "A lovely treat"
+  click_button "Add item"
+end
