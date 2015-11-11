63be5eaa5a44bb9c2de0f9ebf5b66b87788a9708
timkellogg <tim.kellogg@gmail.com>
2015-11-11 12:57:33 -0800

    add region to heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,6 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
