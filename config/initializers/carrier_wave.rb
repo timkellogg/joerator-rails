98a4d5c9ad5846b9db5d02efe2bad80fda4fa8aa
timkellogg <tim.kellogg@gmail.com>
2015-11-11 13:20:47 -0800

    change region for aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -32,7 +32,7 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV['S3_ACCESS_KEY'],
     secret_access_key: ENV['S3_SECRET_KEY'],
-    region:            ENV['S3_REGION'] # Required
+    region:            ENV['S3_REGION'] 
   }
 
   # Optional: Signing of download urls, e.g. for serving private
